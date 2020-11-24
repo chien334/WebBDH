740ce810ef577e0a5aa70855a1615eff203eac66
Lê Quyết Chiến <lequyetchien3498@gmail.com>
2020-11-24 19:04:01 +0700

    update database

---
+++ WebBDH/WebBDH/Models/OrderItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebBDH.Models
+{
+    public partial class OrderItem
+    {
+        public long Id { get; set; }
+        public long? ProductId { get; set; }
+        public long? OrderId { get; set; }
+        public string Sku { get; set; }
+        public double? Price { get; set; }
+        public double? Discount { get; set; }
+        public short? Quantity { get; set; }
+        public string Content { get; set; }
+        public string CreateBy { get; set; }
+        public DateTime? CreateTime { get; set; }
+        public string LastUpdateBy { get; set; }
+        public DateTime? LastUpdateTime { get; set; }
+
+        public virtual UserOrder Order { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
---
+++ WebBDH/WebBDH/Models/UserAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebBDH.Models
+{
+    public partial class UserAccount
+    {
+        public UserAccount()
+        {
+            Cart = new HashSet<Cart>();
+            UserOrder = new HashSet<UserOrder>();
+        }
+
+        public long Id { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool? Sex { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime? RegistedAt { get; set; }
+        public DateTime? LastLogin { get; set; }
+        public bool? Deleted { get; set; }
+        public string CreateBy { get; set; }
+        public DateTime? CreateTime { get; set; }
+        public string LastUpdateBy { get; set; }
+        public DateTime? LastUpdateTime { get; set; }
+
+        public virtual ICollection<Cart> Cart { get; set; }
+        public virtual ICollection<UserOrder> UserOrder { get; set; }
+    }
+}
