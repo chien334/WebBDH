63c619b1ab9a673613165e87e38540cba08be5a7
chien334 <lequyetchien3498@gmail.com>
2020-12-27 11:24:57 +0700

    update login google

--- WebBDH/Web/src/app/Pages/user/login-user/login-user.component.ts
+++ WebBDH/Web/src/app/Pages/user/login-user/login-user.component.ts
@@ -15,16 +15,39 @@ export class LoginUserComponent implements OnInit {
   isLoginFailed = false;
   errorMessage = '';
   roles: string[] = [];
+  public gapiSetup: boolean = false; // marks if the gapi library has been loaded
+  public authInstance: gapi.auth2.GoogleAuth;
+  public error: string;
+  public user: gapi.auth2.GoogleUser;
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService, private router: Router) { }
 
-  constructor(private authService: AuthService, private tokenStorage: TokenStorageService,private router: Router) { }
-
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     if (this.tokenStorage.getToken()) {
       this.isLoggedIn = true;
       this.roles = this.tokenStorage.getUser().roles;
     }
+    if (await this.checkIfUserAuthenticated()) {
+      this.user = this.authInstance.currentUser.get();
+    }
   }
+  async initGoogleAuth(): Promise<void> {
+    //  Create a new Promise where the resolve
+    // function is the callback passed to gapi.load
+    const pload = new Promise((resolve) => {
+      gapi.load('auth2', resolve);
+    });
 
+    // When the first promise resolves, it means we have gapi
+    // loaded and that we can call gapi.init
+    return pload.then(async () => {
+      await gapi.auth2
+        .init({ client_id: '1074242255151-c8f61jtakt68o9nsdqfd3bp5qlmfosjn.apps.googleusercontent.com' })
+        .then(auth => {
+          this.gapiSetup = true;
+          this.authInstance = auth;
+        });
+    });
+  }
   onSubmit(): void {
     this.authService.loginUser(this.form).subscribe(
       data => {
@@ -46,5 +69,43 @@ export class LoginUserComponent implements OnInit {
   reloadPage(): void {
     window.location.reload();
   }
+  async authenticate(): Promise<gapi.auth2.GoogleUser> {
+    // Initialize gapi if not done yet
+    if (!this.gapiSetup) {
+      await this.initGoogleAuth();
+    }
 
+    // Resolve or reject signin Promise
+    return new Promise(async () => {
+      await this.authInstance.signIn().then(
+        user => {
+          this.user = user;
+          const baseRequest = {
+            userName: this.user?.getBasicProfile().getEmail(),
+            email: this.user?.getBasicProfile().getEmail(),
+            firstName: this.user?.getBasicProfile().getFamilyName(),
+            lastName: this.user?.getBasicProfile().getGivenName()
+          };
+          this.authService.register(baseRequest).subscribe(
+            data => {
+              console.log(data);
+            },
+            err => {
+              console.log(err);
+            }
+          );
+          this.isLoggedIn = true;
+          this.router.navigateByUrl('/home');
+        },
+        error => this.error = error);
+    });
+  }
+  async checkIfUserAuthenticated(): Promise<boolean> {
+    // Initialize gapi if not done yet
+    if (!this.gapiSetup) {
+      await this.initGoogleAuth();
+    }
+
+    return this.authInstance.isSignedIn.get();
+  }
 }
